777bd2ecddded6a6f06f022a02e26ec0a28b42ab
Kevin <kevinastefano@gmail.com>
2020-03-21 01:01:55 +0700

    BnB done ~Fano

--- Puzzle15.py
+++ Puzzle15.py
@@ -1,7 +1,7 @@
 #Name   : Kevin Austin Stefano
 #NIM    : 13518104
 
-
+import collections 
 
 class Matrix(object) :
     def __init__ (self):
@@ -64,7 +64,7 @@ def checkReachableGoal (matriks):
                 stop=0
             while (m<4 and stop==0):
                 while (n<4):
-                    if (matriks[m][n]<matriks[k][l] and matriks[m][n]!=0 and matriks[k][l]!=0):
+                    if (matriks[m][n]<matriks[k][l] and matriks[m][n]!=0) or matriks[k][l]==0:
                         kurang=kurang+1
                     n=n+1
                 n=0
@@ -73,25 +73,118 @@ def checkReachableGoal (matriks):
     kurang = kurang+x
     #check apakah genap atau bukan
     if kurang%2==0:
-        return 1; #bisa
+        return 1 #bisa
     else:
         return 0 #tidakbisa
             
             
+def checkDenganSusunanAkhir(matriks):
+    count = 0
+    check = 1
+    for i in range(4):
+        for j in range(4):
+            if matriks[i][j]!=0 and matriks[i][j] != check:
+                count=count+1
+            check = (check+1)%16
+    return count
+    
 
-#MAIN           
+def whereKotakKosong(matriks):
+    for i in range(4):
+        for j in range(4):
+            if matriks[i][j]==0:
+                break;
+        if matriks[i][j]==0:
+            break;
+    return i,j
+
+def AddQueue(queue, idxi, idxj, cost):
+    if queue:
+        i,j,c = queue[0]
+        if c>=cost:
+            queue.appendleft((idxi,idxj,cost))
+        else:
+            queue.append((idxi,idxj,cost))
+    else:
+        queue.append((idxi,idxj,cost))
+
+def swipeAndCheck(matriks, queue, kosongI, kosongJ, swipeI, swipeJ,langkah,savedArray):
+    cost = 0
+    
+    if (swipeI==kosongI-1 and swipeJ==kosongJ) or (swipeI==kosongI+1 and swipeJ==kosongJ) or (swipeJ==kosongJ+1 and swipeI==kosongI) or (swipeJ==kosongJ-1 and swipeI==kosongI):
+        matriks[kosongI][kosongJ] =matriks[swipeI][swipeJ]
+        matriks[swipeI][swipeJ]=0
+        cost = checkDenganSusunanAkhir(matriks)+langkah
+        AddQueue(queue,swipeI,swipeJ,cost)
+        savedArray.append(matriks)
+        matriks[swipeI][swipeJ]=matriks[kosongI][kosongJ]
+        matriks[kosongI][kosongJ] = 0
+    
+def checkUDLRandAddQueue(matriks, queue, langkah, savedArray):
+    i,j=whereKotakKosong(matriks)
+    kosongI,kosongJ,cost= queue[0]
+    temp =matriks[kosongI][kosongJ]
+    matriks[kosongI][kosongJ]=0
+    matriks[i][j] = temp
+    #printMatriks(matriks)
+    
+    #print(queue)
+    if 1+1==2:
+    #matriks in savedArray:
+     #   queue.popleft()
+     #   return 0
+    #else:
+        count =checkDenganSusunanAkhir(matriks)
+        if count==0:
+            return 1
+        else:
+            #print(queue)
+            queue.popleft()
+
+            #checkjika UP
+            if (kosongI-1>=0):
+                swipeAndCheck(matriks,queue,kosongI,kosongJ,kosongI-1,kosongJ,langkah,savedArray)
+            
+            #checkjika DOWN
+            if (kosongI+1<=3):
+                swipeAndCheck(matriks,queue,kosongI,kosongJ,kosongI+1,kosongJ,langkah,savedArray)
+                
+            #checkjika LEFT
+            if (kosongJ-1>=0):
+                swipeAndCheck(matriks,queue,kosongI,kosongJ,kosongI,kosongJ-1,langkah,savedArray)
+           
+            
+            #checkjika RIGHT
+            if (kosongJ+1<=3):
+                swipeAndCheck(matriks,queue,kosongI,kosongJ,kosongI,kosongJ+1,langkah,savedArray)
+            return 0   
+    
+#MAIN
 #Masukan file eksternal
 with open('datainput.txt', 'r') as f:
     matriks = [[int(idx) for idx in line.split(' ')] for line in f]
     if len(matriks)!= 4 or len(matriks[0])  != 4:
         print("Masukan file matriks harus berukuran 4x4");
     else:
-        #matrixA = Matrix()
-        #matrixA[0][1] = 5
-        printMatriks(matriks)
 
         if (checkReachableGoal(matriks)==0):
             print("Matriks tidak bisa di proses")
+        else:
+            
+            i=1
+            baris,kolom = whereKotakKosong(matriks)
+            queue = collections.deque([(baris,kolom,0)])
+            
+            savedArray = matriks
+            while(queue):
+                if (checkUDLRandAddQueue(matriks,queue,i,savedArray)==1):
+                    break
+                i=i+1
+
+         
+
+            
+            
      
             
     
